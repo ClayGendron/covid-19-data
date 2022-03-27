0273a0ab110cb795e4101a720276bcf9c49077aa
Clay Gendron <46005933+ClayGendron@users.noreply.github.com>
2022-03-27 18:53:07 -0400

    Updated R

--- R/covid.R
+++ R/covid.R
@@ -1,3 +1,11 @@
+library(tidyverse)
+library(tidymodels)
+library(dplyr)
+library(skimr)
 library(here)
 
-print("Hello World")
+
+usa_counties_demographics_url <- "https://github.com/ClayGendron/datasets/blob/main/data/USA_Counties_Demographics.csv"
+
+usa_counties_demographics <- read_csv("https://raw.githubusercontent.com/ClayGendron/datasets/main/data/USA_Counties_Demographics.csv")
+usa_counties_general_elections <- read_csv("https://raw.githubusercontent.com/ClayGendron/datasets/main/data/USA_Counties_Presidential_Elections_2000_2020.csv")
